26699f476a33a40fd2e7a5766db85ade35d5b749
Mohamed Abdiqadir Osman <mohaaosman12@gmail.com>
2019-11-04 11:26:11 +0300

    Add project files.

---
+++ XamarinApp2/XamarinApp2/Models/Article.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinApp2.Models
+{
+    class Article
+    {
+        [JsonProperty("userId", NullValueHandling = NullValueHandling.Ignore)]
+        public long? UserId { get; set; }
+
+        [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Id { get; set; }
+
+        [JsonProperty("title", NullValueHandling = NullValueHandling.Ignore)]
+        public string Title { get; set; }
+
+        [JsonProperty("body", NullValueHandling = NullValueHandling.Ignore)]
+        public string Body { get; set; }
+    }
+    public class Source
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ XamarinApp2/XamarinApp2/Views/NewsArticles.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinApp2.Models;
+
+namespace XamarinApp2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewsArticles : ContentPage
+    {
+        private HttpClient _client = new HttpClient();
+
+        public NewsArticles()
+        {
+            InitializeComponent();
+            GetNews();
+        }
+
+        private async void GetNews()
+        {
+            var current = Connectivity.NetworkAccess;
+
+            if (current == NetworkAccess.Internet)
+            {
+                // Call http
+                var content = await _client.GetStringAsync("https://jsonplaceholder.typicode.com/posts");
+
+                var ar = JsonConvert.DeserializeObject<ObservableCollection<Article>>(content);
+
+                ObservableCollection<Article> articles = new ObservableCollection<Article>();
+
+                foreach (var item in ar)
+                {
+                    if (articles.All(a => a.Id != item.Id))
+                    {
+                        articles.Add(item);
+                    }
+                }
+
+
+
+                articlelist.ItemsSource = articles;
+
+
+
+            }
+        }
+
+        private void articlelist_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+    }
+}
